75d34f17f8a019732777d7005dd477940217b9b7
Sam Uong <samuong@gmail.com>
2020-06-30 17:09:10 -0400

    Initial commit

---
+++ alpaca.rb
@@ -0,0 +1,11 @@
+class Alpaca < Formula
+    desc "A local HTTP proxy for command-line tools. Supports PAC scripts and NTLM authentication."
+    homepage "https://github.com/samuong/alpaca"
+    url "https://github.com/samuong/alpaca/releases/download/v1.1.0/darwin-amd64"
+    sha256 "e8ab7248e00ab3fcdff6a94444ea7fe408287dfd077f303a4fc4805eed85605a"
+    version "1.1.0"
+
+    def install
+        bin.install "alpaca"
+    end
+end
