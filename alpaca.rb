2f4cd969cd915b43c697eb7c698275006e5a44f7
samhaque <samhaque@users.noreply.github.com>
2022-04-18 23:22:36 -0400

    testing apple silicon tap

--- alpaca.rb
+++ alpaca.rb
@@ -1,12 +1,24 @@
 class Alpaca < Formula
     desc "A local HTTP proxy for command-line tools. Supports PAC scripts and NTLM authentication."
     homepage "https://github.com/samuong/alpaca"
-    url "https://github.com/samuong/alpaca/releases/download/v1.2.0/alpaca_v1.2.0_darwin-amd64"
-    sha256 "d21e2119e907a212e7b3ea28a6b9e7fd2723dc44be94b2d6c4b5c13398078172"
+    if OS.mac? && Hardware::CPU.intel?
+        url "https://github.com/samuong/alpaca/releases/download/v1.2.0/alpaca_v1.2.0_darwin-amd64"
+        sha256 "d21e2119e907a212e7b3ea28a6b9e7fd2723dc44be94b2d6c4b5c13398078172"
+    end
+    if OS.mac? && !Hardware::CPU.intel?
+        url "https://github.com/samuong/alpaca/releases/download/v1.2.0/alpaca_v1.2.0_darwin-arm64"
+        sha256 "0231da653167609b77ee3ee8d2542bf700a85cdb3270aa72e8c2fc91e761bc84"
+    end
     version "1.2.0"
 
     def install
-        bin.install "alpaca_v1.2.0_darwin-amd64" => "alpaca"
+        if OS.mac? && Hardware::CPU.intel?
+            bin.install "alpaca_v1.2.0_darwin-amd64" => "alpaca"
+        
+        end
+        if OS.mac? && !Hardware::CPU.intel?
+            bin.install "alpaca_v1.2.0_darwin-arm64" => "alpaca"
+        end
     end
 
     plist_options :startup => true
