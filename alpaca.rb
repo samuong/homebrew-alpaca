7af5f120c70163ecd089f503c12b8bf5b4ebaa31
Sam Uong <samuong@gmail.com>
2023-02-24 13:45:16 +1100

    Update to v2.0.0

--- alpaca.rb
+++ alpaca.rb
@@ -2,22 +2,22 @@ class Alpaca < Formula
     desc "A local HTTP proxy for command-line tools. Supports PAC scripts and NTLM authentication."
     homepage "https://github.com/samuong/alpaca"
     if OS.mac? && Hardware::CPU.intel?
-        url "https://github.com/samuong/alpaca/releases/download/v1.3.2/alpaca_v1.3.2_darwin-amd64"
-        sha256 "7e7941c0642d582305b768c587bddb29236f1a0d6cb27125a05725da7d0c3e74"
+        url "https://github.com/samuong/alpaca/releases/download/v2.0.0/alpaca_v2.0.0_darwin-amd64"
+        sha256 "a63b23a3ea3df130d4cab9e122d033d7f4a1b532b0cb7c80aa94d68227c63eba"
     end
     if OS.mac? && !Hardware::CPU.intel?
-        url "https://github.com/samuong/alpaca/releases/download/v1.3.2/alpaca_v1.3.2_darwin-arm64"
-        sha256 "99865686fbb2b17d24ecb9f19dc846282c4d6049c3bae771118819b2e04699df"
+        url "https://github.com/samuong/alpaca/releases/download/v2.0.0/alpaca_v2.0.0_darwin-arm64"
+        sha256 "ae98d17d0e4a2ba5660d64f79a02795810f1f5dae870ce16be4f7815cdf6bbe1"
     end
-    version "1.3.2"
+    version "2.0.0"
 
     def install
         if OS.mac? && Hardware::CPU.intel?
-            bin.install "alpaca_v1.3.2_darwin-amd64" => "alpaca"
+            bin.install "alpaca_v2.0.0_darwin-amd64" => "alpaca"
         
         end
         if OS.mac? && !Hardware::CPU.intel?
-            bin.install "alpaca_v1.3.2_darwin-arm64" => "alpaca"
+            bin.install "alpaca_v2.0.0_darwin-arm64" => "alpaca"
         end
     end
 
