b1fc3c9961485938fa9d60b0e10ea24f350c1c49
Sam Uong <samuong@gmail.com>
2023-02-24 14:18:19 +1100

    Replace deprecated call to plist_options with a service block
    Fixes #3

--- alpaca.rb
+++ alpaca.rb
@@ -20,31 +20,10 @@ class Alpaca < Formula
             bin.install "alpaca_v2.0.0_darwin-arm64" => "alpaca"
         end
     end
-
-    plist_options :startup => true
-
-    def plist
-        <<~EOS
-            <?xml version="1.0" encoding="UTF-8"?>
-            <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-            <plist version="1.0">
-                <dict>
-                    <key>Label</key>
-                    <string>#{plist_name}</string>
-                    <key>ProgramArguments</key>
-                    <array>
-                        <string>#{opt_bin}/alpaca</string>
-                    </array>
-                    <key>KeepAlive</key>
-                    <false/>
-                    <key>RunAtLoad</key>
-                    <true/>
-                    <key>StandardErrorPath</key>
-                    <string>/dev/null</string>
-                    <key>StandardOutPath</key>
-                    <string>/dev/null</string>
-                </dict>
-            </plist>
-        EOS
+    
+    service do
+        run opt_bin/alpaca
+        log_path var/"log/alpaca.log"
+        error_log_path var/"log/alpaca-error.log"
     end
 end
